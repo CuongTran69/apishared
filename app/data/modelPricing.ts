709eb1283dd9195c058a864a22810a1209a02984
Cường Trần <cuongtran17796@gmail.com>
2025-05-12 14:27:09 +0700

    update

--- app/data/modelPricing.ts
+++ app/data/modelPricing.ts
@@ -16,92 +16,227 @@ export interface ModelPricing {
   [category: string]: ModelInfo[];
 }
 
-const modelPricing: ModelPricing = {
-  "OpenAI": [
+const modelPricing = {
+  "Anthropic Models": [
     {
-      apiName: "gpt-4o",
-      realName: "GPT-4o",
-      inputPrice: { original: 10.0, discounted: 5.0 },
-      outputPrice: { original: 30.0, discounted: 15.0 }
+      apiName: 'anthropic:3.7-sonnet-search',
+      realName: 'Claude 3.7 Search',
+      inputPrice: { original: 3.75, discounted: 1.88 },
+      outputPrice: { original: 18.75, discounted: 9.38 }
     },
     {
-      apiName: "gpt-4-turbo",
-      realName: "GPT-4 Turbo",
-      inputPrice: { original: 10.0, discounted: 5.0 },
-      outputPrice: { original: 30.0, discounted: 15.0 }
+      apiName: 'anthropic:3.7-sonnet-thinking-search',
+      realName: 'Claude 3.7 Thinking Search',
+      inputPrice: { original: 5.00, discounted: 2.50 },
+      outputPrice: { original: 25.00, discounted: 12.5 }
     },
     {
-      apiName: "gpt-4-vision",
-      realName: "GPT-4 Vision",
-      inputPrice: { original: 10.0, discounted: 5.0 },
-      outputPrice: { original: 30.0, discounted: 15.0 }
+      apiName: 'anthropic:3.7-sonnet',
+      realName: 'Claude 3.7 Sonnet',
+      inputPrice: { original: 3.75, discounted: 1.88 },
+      outputPrice: { original: 18.75, discounted: 9.38 }
     },
     {
-      apiName: "gpt-4",
-      realName: "GPT-4",
-      inputPrice: { original: 30.0, discounted: 15.0 },
-      outputPrice: { original: 60.0, discounted: 30.0 }
+      apiName: 'anthropic:3.7-sonnet-thinking',
+      realName: 'Claude 3.7 Thinking Mode Extended',
+      inputPrice: { original: 5.00, discounted: 2.50 },
+      outputPrice: { original: 25.00, discounted: 12.5 }
     },
     {
-      apiName: "gpt-3.5-turbo",
-      realName: "GPT-3.5 Turbo",
-      inputPrice: { original: 1.0, discounted: 0.5 },
-      outputPrice: { original: 2.0, discounted: 1.0 }
+      apiName: 'anthropic:3.5-sonnet-search',
+      realName: 'claude-3.5-sonnet-20241022',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 15.00, discounted: 7.5 }
+    },
+    {
+      apiName: 'anthropic:3.5-sonnet-20241022',
+      realName: 'claude-3.5-sonnet-20241022',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 15.00, discounted: 7.5 }
+    },
+    {
+      apiName: 'anthropic:3.5-sonnet-20241022-think-exp',
+      realName: 'claude-3.5-sonnet-20241022-think-exp',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 15.00, discounted: 7.5 }
+    },
+    {
+      apiName: 'anthropic:3-opus',
+      realName: 'Claude-3-opus',
+      inputPrice: { original: 15.00, discounted: 7.5 },
+      outputPrice: { original: 75.00, discounted: 37.5 }
+    },
+    {
+      apiName: 'anthropic:3-opus-think-exp',
+      realName: 'Claude-3-opus-think-exp',
+      inputPrice: { original: 15.00, discounted: 7.5 },
+      outputPrice: { original: 75.00, discounted: 37.5 }
+    },
+    {
+      apiName: 'anthropic:3.5-sonnet',
+      realName: 'claude-3.5-sonnet',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 15.00, discounted: 7.5 }
+    },
+    {
+      apiName: 'anthropic:3.5-sonnet-think-exp',
+      realName: 'claude-3.5-sonnet-think-exp',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 15.00, discounted: 7.5 }
+    },
+    {
+      apiName: 'anthropic:3.5-haiku',
+      realName: 'claude-3.5-haiku',
+      inputPrice: { original: 1.00, discounted: 0.5 },
+      outputPrice: { original: 5.00, discounted: 2.5 }
+    },
+  ],
+  "OpenAI Models": [
+    {
+      apiName: 'openai:o4-mini',
+      realName: 'OpenAI o4 mini',
+      inputPrice: { original: 1.10, discounted: 0.55 },
+      outputPrice: { original: 4.40, discounted: 2.20 }
+    },
+    {
+      apiName: 'openai:gpt-4.1',
+      realName: 'OpenAI 4.1',
+      inputPrice: { original: 2.00, discounted: 1.00 },
+      outputPrice: { original: 8.00, discounted: 8.00 }
+    },
+    {
+      apiName: 'openai:o1',
+      realName: 'OpenAI o1',
+      inputPrice: { original: 15.00, discounted: 7.50 },
+      outputPrice: { original: 60.00, discounted: 30.00 }
+    },
+    {
+      apiName: 'openai:o3-mini',
+      realName: 'OpenAI o3-mini',
+      inputPrice: { original: 2.20, discounted: 1.10 },
+      outputPrice: { original: 8.80, discounted: 4.40 }
+    },
+    {
+      apiName: 'openai:o3-mini-think-exp',
+      realName: 'OpenAI o3-mini',
+      inputPrice: { original: 2.20, discounted: 1.10 },
+      outputPrice: { original: 8.80, discounted: 4.40 }
+    },
+    {
+      apiName: 'openai:gpt-4o',
+      realName: 'OpenAI 4o',
+      inputPrice: { original: 2.50, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
+    {
+      apiName: 'openai:gpt-4o-search',
+      realName: 'OpenAI 4o search',
+      inputPrice: { original: 2.50, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
+    {
+      apiName: 'openai:gpt-4o-mini',
+      realName: 'OpenAI 4o-mini',
+      inputPrice: { original: 0.15, discounted: 0.075 },
+      outputPrice: { original: 0.60, discounted: 0.3 }
+    },
+    {
+      apiName: 'openai:gpt-4o-2024-08-06',
+      realName: 'OpenAI 4o-2024-08-06',
+      inputPrice: { original: 2.50, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
+    {
+      apiName: 'openai:o1-mini',
+      realName: 'OpenAI o1-mini',
+      inputPrice: { original: 3.00, discounted: 1.5 },
+      outputPrice: { original: 12.00, discounted: 6.00 }
+    },
+    {
+      apiName: 'openai:gpt-4',
+      realName: 'OpenAI gpt-4',
+      inputPrice: { original: 30.00, discounted: 15.00 },
+      outputPrice: { original: 60.00, discounted: 30.00 }
+    },
+    {
+      apiName: 'openai:gpt-4-turbo-2024-04-09',
+      realName: 'OpenAI gpt-4-turbo-2024-04-09',
+      inputPrice: { original: 10.00, discounted: 5.00 },
+      outputPrice: { original: 30.00, discounted: 15.00 }
     }
   ],
-  "Anthropic": [
+  "DeepSeek Models": [
+    // {
+    //   apiName: 'deepseek:deepseek-v3-0324',
+    //   realName: 'Deepseek v3 03-2024',
+    //   inputPrice: { original: 0.5, discounted: 0.25 },
+    //   outputPrice: { original: 1.5, discounted: 0.75 }
+    // },
     {
-      apiName: "claude-3-opus",
-      realName: "Claude 3 Opus",
-      inputPrice: { original: 15.0, discounted: 7.5 },
-      outputPrice: { original: 75.0, discounted: 37.5 }
+      apiName: 'deepseek:deepseek-r1-search',
+      realName: 'Deepseek R1 search',
+      inputPrice: { original: 0.80, discounted: 0.40 },
+      outputPrice: { original: 2.40, discounted: 1.20 }
     },
     {
-      apiName: "claude-3-sonnet",
-      realName: "Claude 3 Sonnet",
-      inputPrice: { original: 3.0, discounted: 1.5 },
-      outputPrice: { original: 15.0, discounted: 7.5 }
-    },
-    {
-      apiName: "claude-3-haiku",
-      realName: "Claude 3 Haiku",
-      inputPrice: { original: 0.25, discounted: 0.125 },
-      outputPrice: { original: 1.25, discounted: 0.625 }
+      apiName: 'deepseek:deepseek-r1',
+      realName: 'Deepseek Reasoner',
+      inputPrice: { original: 0.80, discounted: 0.40 },
+      outputPrice: { original: 2.40, discounted: 1.20 }
     }
   ],
-  "Google": [
+  "Grok Models": [
     {
-      apiName: "gemini-1.5-pro",
-      realName: "Gemini 1.5 Pro",
-      inputPrice: { original: 7.0, discounted: 3.5 },
-      outputPrice: { original: 21.0, discounted: 10.5 }
+      apiName: 'grok:grok-3',
+      realName: 'Grok v3',
+      inputPrice: { original: 3.00, discounted: 1.50 },
+      outputPrice: { original: 15.00, discounted: 7.50 }
     },
     {
-      apiName: "gemini-1.0-pro",
-      realName: "Gemini 1.0 Pro",
-      inputPrice: { original: 3.5, discounted: 1.75 },
-      outputPrice: { original: 10.5, discounted: 5.25 }
+      apiName: 'grok-3-deepsearch',
+      realName: 'Grok v3 Deep search',
+      inputPrice: { original: 1.50, discounted: 0.75 },
+      outputPrice: { original: 7.50, discounted: 3.75 }
     }
+    // ,
+    // {
+    //   apiName: 'grok-3-reasoner',
+    //   realName: 'Grok v3 reasoner',
+    //   inputPrice: { original: 1.50, discounted: 0.75 },
+    //   outputPrice: { original: 7.50, discounted: 3.75 }
+    // }
   ],
-  "Mistral AI": [
+  "Gemini Models": [
     {
-      apiName: "mistral-large",
-      realName: "Mistral Large",
-      inputPrice: { original: 8.0, discounted: 4.0 },
-      outputPrice: { original: 24.0, discounted: 12.0 }
+      apiName: 'gemini:gemini-2.5-pro-exp-03-25',
+      realName: 'Gemini 2.5 Pro',
+      inputPrice: { original: 2.5, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
     },
     {
-      apiName: "mistral-medium",
-      realName: "Mistral Medium",
-      inputPrice: { original: 2.7, discounted: 1.35 },
-      outputPrice: { original: 8.1, discounted: 4.05 }
+      apiName: 'gemini:gemini-2.5-pro-exp-03-25-search',
+      realName: 'Gemini 2.5 Pro Search',
+      inputPrice: { original: 2.5, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
     },
     {
-      apiName: "mistral-small",
-      realName: "Mistral Small",
-      inputPrice: { original: 2.0, discounted: 1.0 },
-      outputPrice: { original: 6.0, discounted: 3.0 }
-    }
+      apiName: 'gemini:gemini-2.0-pro-exp',
+      realName: 'Gemini 2.0',
+      inputPrice: { original: 2.5, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
+    {
+      apiName: 'gemini:gemini-2.0-pro-exp-search',
+      realName: 'Gemini 2.0 Pro Search',
+      inputPrice: { original: 2.5, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
+    {
+      apiName: 'gemini:gemini-2.0-pro-exp-think-exp',
+      realName: 'Gemini 2.0 Deep Think',
+      inputPrice: { original: 2.5, discounted: 1.25 },
+      outputPrice: { original: 10.00, discounted: 5.00 }
+    },
   ]
 };
 
